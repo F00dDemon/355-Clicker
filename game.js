24ff7e5b6def41f3785f76e06a1a505f1efa3f6f
f00d <delossantos.orion@student.greenriver.edu>
2026-04-07 11:18:16 -0700

    upgrades and onload functionality

--- game.js
+++ game.js
@@ -1,8 +1,30 @@
 let score = 0;
 let pointsPerClick = 1;
+let upgradeArray =[{
+    id: 1,
+    name: "help",
+    cost: 10,
+    bonus: 1
+},{
+    id: 2,
+    name: "hammerUpgrade",
+    cost: 25,
+    bonus: 2
+},{
+    id: 3,
+    name: "costcut",
+    cost: 50,
+    bonus: 2
+},{
+    id: 4,
+    name: "promotion",
+    cost: 100,
+    bonus: 3
+}]
 
 const scoreDisplay = document.getElementById("score-display");
 const rateDisplay = document.getElementById("rate-display");
+const upgradesDiv = document.getElementById("upgrades")
 
 function updateDisplay() {
     scoreDisplay.innerText = `Score: ${score}`;
@@ -13,6 +35,12 @@ document.getElementById("click-btn").addEventListener("click", (event) => {
     updateDisplay();
     score += pointsPerClick;
 });
+
+window.addEventListener("load", (event) => {
+    upgradeArray.forEach((el) => {
+        upgradesDiv.innerText += ` ${el.name}`
+    })
+})
 
 updateDisplay();
 
