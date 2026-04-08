8dd5f8362a0b2e78b02e1ccb30b74f8ff1794fef
Lilian <210286008+liliann19@users.noreply.github.com>
2026-04-08 12:55:20 -0700

    added styling and milestone features

--- game.js
+++ game.js
@@ -1,23 +1,23 @@
 let score = 0;
 let pointsPerClick = 1;
-let upgradeArray =[{
+let upgradeArray = [{
     id: 1,
-    name: "help",
+    name: "Hire worker",
     cost: 10,
     bonus: 1
-},{
+}, {
     id: 2,
-    name: "hammerUpgrade",
+    name: "Better Tools",
     cost: 25,
     bonus: 2
-},{
+}, {
     id: 3,
-    name: "costcut",
+    name: "Cement Mixer",
     cost: 50,
     bonus: 2
-},{
+}, {
     id: 4,
-    name: "promotion",
+    name: "Construction Crew",
     cost: 100,
     bonus: 3
 }]
@@ -27,14 +27,12 @@ const rateDisplay = document.getElementById("rate-display");
 const upgradesDiv = document.getElementById("upgrades")
 
 function updateDisplay() {
-    scoreDisplay.innerText = `Score: ${score}`;
-    rateDisplay.innerText = `Points per click: ${pointsPerClick}`;
-
-
+    scoreDisplay.innerText = `Bricks: ${score}`;
+    rateDisplay.innerText = `Bricks per click: ${pointsPerClick}`;
 }
 
 function renderUpgrades() {
-    upgradesDiv.innerHTML=""
+    upgradesDiv.innerHTML = ""
     upgradeArray.forEach((el) => {
         const upgradeSection = document.createElement("div");
 
@@ -42,21 +40,22 @@ function renderUpgrades() {
         // it will check if the specific upgrade is buyable and will just use
         // either one of these HTML blocks to stop the user from buying it.
         // Its not a proper implement but it works.
-        if(score >= el.cost){
+        if (score >= el.cost) {
             //Can afford upgrade
             upgradeSection.innerHTML = `
-            <strong>${el.name}</strong> \n Cost: ${el.cost} \n ${el.bonus} per click
+            <strong>${el.name}</strong> \n Cost: ${el.cost} \n ${el.bonus} bricks per click
             <button onclick="buyUpgrade(${el.id})" class="buybtn">Buy</button>`;
             upgradesDiv.appendChild(upgradeSection);
-        }else{
+        } else {
             //Can't afford upgrade
             upgradeSection.innerHTML = `
-            <strong>${el.name}</strong> \n Cost: ${el.cost} \n ${el.bonus} per click
+            <strong>${el.name}</strong> \n Cost: ${el.cost} \n ${el.bonus} bricks per click
             <button onclick="buyUpgrade(${el.id})" class="buybtn" disabled>Buy</button>`;
             upgradesDiv.appendChild(upgradeSection);
         }
     })
     updateDisplay();
+    updateMilestones();
 }
 
 
@@ -74,5 +73,28 @@ renderUpgrades()
 document.getElementById("click-btn").addEventListener("click", (event) => {
     score += pointsPerClick;
     renderUpgrades();
-    
+
 });
+
+//milestone function 
+function updateMilestones() {
+    const title = document.querySelector("h1");
+    const winMessage = document.getElementById("win");
+
+    if (score >= 200) {
+        document.body.style.backgroundColor = "#aec7a5";
+        title.textContent = "Building Completed!";
+        winMessage.style.display = "block";
+    }
+    else if (score >= 100) {
+        document.body.style.backgroundColor = "#c97b63"; 
+        title.textContent = "Building Taking Shape"
+    } else if (score >= 50) {
+        document.body.style.backgroundColor = "#e8c27d";
+        title.textContent = "Walls Going Up"
+    } else if (score >= 25) {
+        document.body.style.backgroundColor = "#b7c9a8";
+        title.textContent = "Foundation Started"
+    }
+    
+}
